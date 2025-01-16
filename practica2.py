8e0c230dfef79e76c133d54282ac0b4a98f08123
alba <alba.pinelas@gmail.com>
2025-01-16 12:40:50 +0100

    segundo ejercicio  funcional

---
+++ practica2.py
@@ -0,0 +1,11 @@
+import random as r
+vector1=[]
+vector2=[]
+vector3=[]
+for i in range(15):
+    vector1.append(r.randint(0,100))
+    vector2.append(r.randint(0,100))
+    vector3.append(vector1[i]+vector2[i])
+print(vector1)
+print(vector2)
+print(vector3)
